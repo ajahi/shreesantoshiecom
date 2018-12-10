7eade743ed8d201e23ffebe7d63cac74d7b87769
rshnkunwar <rshnkunwar@gmail.com>
2018-12-10 16:17:03 +0545

    Added SitesTableSeeder and tested the site.

---
+++ database/seeds/SitesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Modules\Site\Models\Site;
+use Faker\Factory;
+
+class SitesTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        $faker = Factory::create();
+        for($i=0; $i<10; $i++) {
+            Site::Create([
+                'title'=>$faker->word,
+                'slogan'=>$faker->word,
+                'description'=>$faker->paragraph,
+                'website'=>$faker->word .".com",
+                'email'=>$faker->email,
+                'location'=>$faker->word,
+                'telephone'=>$faker->phoneNumber,
+                'working_days'=>$faker->numberBetween(2,10),
+                'facebook'=>$faker->word,
+                'google'=>$faker->word,
+                'twitter'=>$faker->word,
+                'instagram'=>$faker->word,
+                'linkedin'=>$faker->word,
+                'skype'=>$faker->word,
+                'attributes'=>json_encode(array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5)),
+                'created_at'=>$faker->dateTime,
+                'updated_at'=>$faker->dateTime
+            ]);
+        }
+    }
+}
