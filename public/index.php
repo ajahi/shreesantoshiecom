92630a50bae45b3d38eb51b128fd86ebf6acc5a3
Sushil <sushiljic@gmail.com>
2019-08-04 09:36:16 +0545

    added temp solution for cors on index.php not good solution

--- public/index.php
+++ public/index.php
@@ -49,6 +49,14 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+
+/* temp solution for cors
+*/
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
