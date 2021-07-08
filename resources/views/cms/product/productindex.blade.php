f848c998afe01bbdebd90f3f1c5f1f5236db3278
Lalit Thapa <mlalitthapa@gmail.com>
2021-07-08 22:00:26 +0545

    Add filter form

--- resources/views/cms/product/productindex.blade.php
+++ resources/views/cms/product/productindex.blade.php
@@ -3,81 +3,42 @@
 @section('content')
 <div class="content-wrapper container">
 @include('flash')
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script>
-
-$(document).ready(function(){
-    $('.catfilter').change(function(){
-        var selectedcategory = $('.catfilter').val(); 
-        console.log(selectedcategory);
-            $.ajax({
-            url:'<?php echo url('/product');?>',
-            data:{category_id:selectedcategory},
-            cache:false,
-            success:function(response){
-              console.log(response)
-              
-              alert(selectedcategory);
-              $('#product_data tbody').append(response);
-              }
-        });
-        
-    }); 
-    $('.deletbutton').click(function(){
-      alert('You are going to delet a product.');
-    });
-
-    function fill_data(filter_procat='',filter_status=''){
-      var dataTable=$('#product_data').DataTable({
-        processing:true,
-        serverSide:true,
-        ajax:{
-          url:"<?php echo url('/product')?>",
-          data:{filter_procat:filter_procat,filter_status:filter_status}
-        },
-        columns:[
-          {
-            data:'procatId',
-            name:'procatId'
-          },
-          {
-            data:'title',
-            name:'title'
-          }
-        ]
-      });
-      
-    }
-  });
-  
-</script>
-
-
     <div class="section">
-   <div class="row">
-   <div class="col-md-12">
-   <a href="productcreate" class="btn btn-primary my-2"><i class="fas fa-plus mr-1"></i>Create</a>
-   <span class=" d-inline-block my-2">
-     <select class="form-select custom-select catfilter" name="category" aria-label="filter by category" placeholder="Filter by category">          
-        @foreach($procat as $procat)
-        <option value='{{$procat->id}}'>{{$procat->title}}</option>             
-        @endforeach
-     </select>
-   </span>
-   <span class=" d-inline-block my-2">
-     <select class="form-select custom-select" name="category" aria-label="filter by category" placeholder="Filter by status">          
-        <option value=0>Draft</option>
-        <option value=1>Published</option>             
-     </select>
-   </span>
-   <span class=" d-inline-block my-2">
-     <select class="form-select custom-select" name="category" aria-label="filter by category" placeholder="Filter by status">          
-        <option value=1>In Stock</option>
-        <option value=0>Out Of Stock</option>             
-     </select>
-   </span>
-   </div>
-   </div>
+
+        <div class="row">
+            <div class="col-10">
+                <div class="d-flex justify-content-between">
+                    <form>
+                        <span class=" d-inline-block my-2">
+                            <select class="form-select custom-select catfilter" name="category" aria-label="filter by category" placeholder="Filter by category">
+                                <option value="">Select Category</option>
+                                @foreach($procat as $procat)
+                                    <option value="{{$procat->id}}" {{ $procat->id == request('category') ? 'selected' : '' }}>{{$procat->title}}</option>
+                                @endforeach
+                            </select>
+                        </span>
+                        <span class=" d-inline-block my-2">
+                            <select class="form-select custom-select" name="status" aria-label="filter by category" placeholder="Filter by status">
+                                <option value="">Select Status</option>
+                                <option value="0" {{ request('status') === '0' ? 'selected' : '' }}>Draft</option>
+                                <option value="1" {{ request('status') === '1' ? 'selected' : '' }}>Published</option>
+                            </select>
+                        </span>
+                        <span class=" d-inline-block my-2">
+                            <select class="form-select custom-select" name="stock" aria-label="filter by category" placeholder="Filter by status">
+                                <option value="">Stock Status</option>
+                                <option value="1" {{ request('stock') == "1" ? 'selected' : '' }}>In Stock</option>
+                                <option value="0" {{ request('stock') == "0" ? 'selected' : '' }}>Out Of Stock</option>
+                            </select>
+                        </span>
+                        <div class="d-inline-block">
+                            <button class="btn btn-primary">Filter</button>
+                        </div>
+                    </form>
+                    <a href="productcreate" class="btn btn-primary my-2"><i class="fas fa-plus mr-1"></i>Create</a>
+                </div>
+            </div>
+        </div>
 
         <table class='table' id='product_data'>
             <thead>
@@ -85,12 +46,12 @@ $(document).ready(function(){
                 <th scope='col'>ID</th>
                 <th scope='col'>Title</th>
                 <th scope='col'>Purchase Price</th>
-                <th scope='col'>Stock Quantity</th> 
+                <th scope='col'>Stock Quantity</th>
                 <th scope='col'>Action</th>
                 </tr>
             </thead>
             <p class='response'></p>
-            
+
             @foreach($product as $posts)
             <tbody>
                 <tr>
@@ -98,9 +59,9 @@ $(document).ready(function(){
                 <td scope="col">
                   <img src="{{$posts->url()}}" height="60" width="60"><br/>
                   <a href="/product-detail/{{$posts->slug}}" target="_blank" class='product-name'>{{$posts->title}}</a>
-                  <p class="text-secondary">Categories:<span class="text-dark mr-2"></p> 
+                  <p class="text-secondary">Categories:<span class="text-dark mr-2"></p>
                 </td>
-                
+
                 <td scope="col"><span class="text-secondary mr-2">Purchase Price:</span>Rs. {{$posts->purchase_price}}
                 <div class="py-1">
                 <p class="mb-0"><span class="text-secondary mr-2">Selling Price:</span>Rs. {{$posts->sell_price}}</p>
@@ -126,13 +87,16 @@ $(document).ready(function(){
                     @method('DELETE')
                     <button class="btn deletbutton"><i class="fas fa-trash text-danger"></i></button>
                 </form>
-               
+
                 </td>
                 </tr>
             </tbody>
             @endforeach
         </table>
-       
+
+        <div>
+            {!! $product->appends(request()->all())->links() !!}
+        </div>
     </div>
 </div>
 @endsection
